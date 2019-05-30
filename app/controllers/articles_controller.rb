59c200514e73c00e62e9c5f1e00871a33296fb6b
Grace <gowolabi2@gmail.com>
2019-05-30 09:25:45 +0000

    Edit and show link action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,13 @@
 class ArticlesController < ApplicationController
+    def index
+       @articles = Article.all 
+    end 
+    
     def new
         @article = Article.new
     
     end
+    
     def edit
         @article = Article.find(params[:id])
     end
@@ -33,6 +38,7 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    
     private 
     def article_params
         params.require(:article).permit(:title, :description)
