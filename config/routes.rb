fb9cd57d538b28e69fb00fe6318e3d5419b7731f
Grace <gowolabi2@gmail.com>
2019-06-06 06:39:20 +0000

    User listing

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root  "pages#home"
   get "about", to: "pages#about"
   resources :articles
+  resources :users
   get 'signup', to: 'users#new'
   post 'users', to: 'users#create'
+ 
+  
 end
