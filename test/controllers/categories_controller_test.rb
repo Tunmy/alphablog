7fac345eb3346ca48020bc0358a2a79ef2387145
Grace <gowolabi2@gmail.com>
2019-06-08 08:46:02 +0000

    Fixed test and updated navbar

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
     def setup
         @category = Category.create(name: "Korean Drama")
+        @user = User.create(username: "prudence", email: "prudence@email.com",password: "password", admin: true)
     end
     
     
@@ -11,11 +12,18 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
         assert_response :success
     end
     test 'should get new' do
-        get new_category_path
+        sign_in_as(@user, "password")
+        get new_category_path(@category)
         assert_response :success
     end
     test 'should get show' do
         get category_path(@category)
         assert_response :success
+    end
+    test "should redirect create when admin not logged in" do
+        assert_no_difference 'Category.count' do
+            post categories_path, params: {category: {name: "Korean Drama"}}
+        end
+        assert_redirected_to categories_path
     end
 end
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
+    def setup
+        @user = User.create(username: "prudence", email: "prudence@email.com",password: "password", admin: true)
+    end
+    
 
     test "get new category form and create category" do
+        sign_in_as(@user, "password")
         get new_category_path
-        assert_template 'categories/new'
+        assert_template "categories/new"
         assert_difference 'Category.count', 1 do
             post categories_path, params: { category: { name: "Korean Drama" } }
             follow_redirect!
@@ -13,10 +18,12 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
         assert_match "Korean Drama", response.body
     end
     test 'Invalid category submission results in failure' do 
+        sign_in_as(@user, "password")
         get new_category_path
-        assert_template 'categories/new'
+        assert_template "categories/new"
         assert_no_difference 'Category.count' do
             post categories_path, params: { category: {name: " "} }
+           
         end
         assert_template 'categories/new'
         assert_select 'h2.panel-title'
